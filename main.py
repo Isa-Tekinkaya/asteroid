ddbe5c0c8f419ffb740aacda07b857b31b78a808
Isa Tekinkaya <isatekinkaya@yahoo.de>
2025-04-13 07:04:34 +0300

    Grouping Objects

--- main.py
+++ main.py
@@ -6,27 +6,33 @@ import pygame
 from constants import *
 from player import Player
 def main():
-    p = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 )
     print(f"Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
     pygame.init()
     clock = pygame.time.Clock()
+    p = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 )
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     while True:
         # Calculate delta time (time passed since last frame)
-        dt = clock.tick(60) / 1000  # 60 FPS cap, convert to seconds
-
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         # Clear the screen and redraw everything
         screen.fill("black")
-        p.update(dt)  # Update player with the correct delta time
-        p.draw(screen)  # Draw player
+        updateable.update(dt)  # Update player with the correct delta time
+        for sprite in drawable:  # Draw player
+            sprite.draw(screen)
+                               
 
         pygame.display.flip() # Update the screen
-        
+        dt = clock.tick(60) / 1000  # 60 FPS cap, convert to seconds
+
 if __name__ == "__main__":
     main()
